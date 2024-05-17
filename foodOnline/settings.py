64202667a209a61f30df8f55fd738678e1e471bc
Deepanshu Sharma <sdipanshu746@gamil.com>
2024-05-17 13:15:11 +0530

    forgot password feature

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -159,3 +159,16 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# Email configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER ='sdipanshu746@gmail.com' 
+EMAIL_HOST_PASSWORD ="ufiryomeymndzwzi"
+
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <sdipanshu746@gmail.com>'
