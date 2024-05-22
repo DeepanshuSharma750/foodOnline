3daac89688be2f8e4ad412244c32eeb78c6d7368
Deepanshu Sharma <sdipanshu746@gamil.com>
2024-05-22 23:28:25 +0530

    cart functionality

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
 from vendor.models import Vendor
+from django.template.defaultfilters import slugify
 
 
 # Restrict the vendor from accessing the customer page
@@ -97,7 +98,7 @@ def registerVendor(request):
             vendor = v_form.save(commit=False)
             vendor.user = user
             vendor_name = v_form.cleaned_data['vendor_name']
-        
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
--- foodOnline/urls.py
+++ foodOnline/urls.py
@@ -17,15 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+
+    path('marketplace/', include('marketplace.urls')),
+
+    # CART
+    path('cart/', MarketplaceViews.cart, name='cart'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
